81fddcde44b75d7ec284c4ecc263930d147f2123
Connor Lamont <c_amont@live.concordia.ca>
2021-02-10 11:15:43 -0500

    Added greyscale functionality and footer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let greyScale = true;
+let rainbow = false;
 makeGrid(10);
 
 function makeGrid(num) {
@@ -18,40 +20,85 @@ document.querySelector('.grid-btn').addEventListener('click', () => {
   makeGrid(gridSize);
 });
 
-// Rainbow button functionality
+
 document.querySelector('.rainbow').addEventListener('click', () => {
-  const squares = document.querySelectorAll('.grid-square');
-  squares.forEach(square => {
-    clearSquare(square);
-    square.addEventListener('mouseover', (e) => {
-      makeRainbow(e.target);
-    });
-  });
+  rainbow = true;
+  greyScale = false;
+  const grid = document.querySelector('.grid-container');
+  let gridSize = Math.sqrt(grid.childElementCount);
+  removeChildren(grid);
+  makeGrid(gridSize);
 });
 
 // Grey-scale button functionality
+// document.querySelector('.grey-scale').addEventListener('click', () => {
+//   rainbow = false;
+//   greyScale = true;
+//   const squares = document.querySelectorAll(`.grid-square`);
+//   squares.forEach(square => {
+//     clearSquare(square);
+//     square.addEventListener(`mouseover`, e => {
+//       makeDarker(e.target);
+//     });
+//   });
+// });
+
 document.querySelector('.grey-scale').addEventListener('click', () => {
-  const squares = document.querySelectorAll(`.grid-square`);
-  squares.forEach(square => {
-    clearSquare(square);
-    square.addEventListener(`mouseover`, e => {
-      makeBlack(e.target);
-    });
-  });
+  rainbow = false;
+  greyScale = true;
+  const grid = document.querySelector('.grid-container');
+  let gridSize = Math.sqrt(grid.childElementCount);
+  removeChildren(grid);
+  makeGrid(gridSize);
 });
 
 function makeSquare() {
   const grid = document.querySelector('.grid-container');
   let newDiv = document.createElement('div');
-  newDiv.classList.add('grid-square');
+  newDiv.classList.add('grid-square', '0');
+  newDiv.style.backgroundColor = 'rgb(255, 255, 255)';
   newDiv.addEventListener(`mouseover`, (e) => {
-    makeBlack(e.target);
+    if(greyScale) {
+      makeDarker(e.target);
+    } else {
+      makeRainbow(e.target);
+    }
   });
   grid.appendChild(newDiv);
 }
 
-function makeBlack(div) {
-  div.style.backgroundColor = `black`;
+function makeDarker(div) {
+  if (div.classList.contains('0')) {
+    div.style.backgroundColor = `hsl(0, 0%, 90%)`;
+    updateClass(div, '0', '1');
+  } else if (div.classList.contains('1')) {
+    div.style.backgroundColor = `hsl(0, 0%, 80%)`;
+    updateClass(div, '1', '2');
+  } else if (div.classList.contains('2')) {
+    div.style.backgroundColor = `hsl(0, 0%, 70%)`;
+    updateClass(div, '2', '3');
+  } else if (div.classList.contains('3')) {
+    div.style.backgroundColor = `hsl(0, 0%, 60%)`;
+    updateClass(div, '3', '4');
+  } else if (div.classList.contains('4')) {
+    div.style.backgroundColor = `hsl(0, 0%, 50%)`;
+    updateClass(div, '4', '5');
+  } else if (div.classList.contains('5')) {
+    div.style.backgroundColor = `hsl(0, 0%, 40%)`;
+    updateClass(div, '5', '6');
+  } else if (div.classList.contains('6')) {
+    div.style.backgroundColor = `hsl(0, 0%, 30%)`;
+    updateClass(div, '6', '7');
+  } else if (div.classList.contains('7')) {
+    div.style.backgroundColor = `hsl(0, 0%, 20%)`;
+    updateClass(div, '7', '8');
+  } else if (div.classList.contains('8')) {
+    div.style.backgroundColor = `hsl(0, 0%, 10%)`;
+    updateClass(div, '8', '9');
+  } else if (div.classList.contains('9')) {
+    div.style.backgroundColor = `hsl(0, 0%, 0%)`;
+    updateClass(div, '9', '10');
+  }
 }
 
 function removeChildren(parent) {
@@ -60,24 +107,24 @@ function removeChildren(parent) {
   }
 }
 
-function getLightness(div) {
-  const rgbString = div.getPropertyValue('background-color');
-  console.log(rgbString);
-}
 
-function makeRainbow(div) {
-  div.style.backgroundColor = `rgb(${randomChoice()}, ${randomChoice()}, ${randomChoice()})`;
-}
 
-function makeGreyScale(div) {
-  
-}
 
 // Get random value 0-255
 function randomChoice() {
   return Math.floor(Math.random() * 256);
 }
+function makeRainbow(div) {
+  div.style.backgroundColor = `rgb(${randomChoice()}, ${randomChoice()}, ${randomChoice()})`;
+}
+
 
 function clearSquare(div) {
-  div.style.backgroundColor = `#fff`;
+  div.style.backgroundColor = `hsl(0, 0%, 100%)`;
+  div.classList[1] = '0';
+}
+
+function updateClass(div, oldClass, newClass) {
+  div.classList.remove(oldClass);
+  div.classList.add(newClass);
 }
